cf67a6984a2954320ca8cd48390cac3e8f7cbca8
JRodriguezZ <jrodriguezz00@elpuig.xeill.net>
2020-12-10 18:17:16 +0100

    Initial Commit

--- app/src/main/java/com/example/createrapp/MainActivity.java
+++ app/src/main/java/com/example/createrapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.createrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.example.createrapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+        Glide.with(this).load(R.drawable.mood).circleCrop().into(binding.perfil);
     }
 }

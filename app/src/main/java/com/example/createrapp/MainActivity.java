f0868375ae8b3b5cc513cacf9e531e1d9f06accc
JRodriguezZ <jrodriguezz00@elpuig.xeill.net>
2020-12-11 19:57:51 +0100

    Day2

--- app/src/main/java/com/example/createrapp/MainActivity.java
+++ app/src/main/java/com/example/createrapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.createrapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.example.createrapp.databinding.ActivityMainBinding;
@@ -22,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_fragment)).getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavView, navController);
 
-        Glide.with(this).load(R.drawable.mood).circleCrop().into(binding.perfil);
+        Glide.with(this).load(R.drawable.mood).circleCrop().into(binding.profilePicture); /* Imagen de perfil redondeada */
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.nuevoDesafioFragment || destination.getId() == R.id.actividadFragment) {
+                binding.searchView.setVisibility(View.GONE);
+            } else {
+                binding.searchView.setVisibility(View.VISIBLE);
+            }
+
+            if (destination.getId() == R.id.profileFragment) {
+                binding.profilePicture.setVisibility(View.GONE);
+            } else {
+                binding.profilePicture.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.profilePicture.setOnClickListener(v -> {
+            navController.navigate(R.id.profileFragment);
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/createrapp/Publicacion.java
@@ -0,0 +1,9 @@
+package com.example.createrapp;
+
+public class Publicacion {
+   String titulo;
+   String fecha;
+   String multimedia;
+   String dificultad;
+
+}
